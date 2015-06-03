17141c2b756ca3db13c86d5bec96189fef453505
cynthiakellogg <cynthiakellogg@gmail.com>
2015-06-03 11:03:24 -0700

    working on adding pipe

--- site/js/flappy_bird.js
+++ site/js/flappy_bird.js
@@ -1,8 +1,10 @@
 var graphicsSystem = require('./systems/graphics');
 var bird = require('./entities/bird');
+var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
 	this.entities = [new bird.Bird()];
+	this.entities = [new pipe.Pipe()];
 	this.graphics = new graphicsSystem.graphicsSystem(this.entities);
 };
 
